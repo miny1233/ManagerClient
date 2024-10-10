0785c8f1f5b3354c979f7123411e5bcf1342da4c
miny1233 <1596784863@qq.com>
2024-10-10 18:34:59 +0800

    添加头像

--- ManagerClient/Program.cs
+++ ManagerClient/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<BookService>();
+builder.Services.AddAntDesign();
 
 var app = builder.Build();
 
