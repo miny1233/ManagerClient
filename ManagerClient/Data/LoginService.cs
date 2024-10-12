b6b4c16a90c9ac64eb89b41ff5194e160184b1dd
miny1233 <1596784863@qq.com>
2024-10-12 16:47:43 +0800

    邮件验证码

--- ManagerClient/Data/LoginService.cs
+++ ManagerClient/Data/LoginService.cs
@@ -1,5 +1,9 @@
 using System.Buffers.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Text.Encodings.Web;
+using AntDesign.Filters;
 using AspNetCore.Totp;
 using QRCoder;
 
@@ -8,17 +12,13 @@ namespace ManagerClient.Data {
 
         readonly static string secretKey = Guid.NewGuid().ToString();
         readonly TotpGenerator generator = new();
-        public bool HasOTP = false;
+        // public bool HasOTP = false;
 
         private string otp = "";
 
         public LoginService()
         {
-            var code = CreateAndShowOTP();
-
-            Console.WriteLine("====================================");
-            Console.WriteLine("Your New OTP Code is " + code);
-            Console.WriteLine("====================================");
+            // _ = CreateAndShowOTP();
         }
 
         public bool CheckOTP(string code)
@@ -26,11 +26,66 @@ namespace ManagerClient.Data {
             return otp == code;
         }
 
-       public string CreateAndShowOTP()
+       public void CreateAndShowOTP()
        {
-            HasOTP = true;
-            otp = generator.Generate(secretKey).ToString();
-            return otp;
+            var code = generator.Generate(secretKey).ToString();
+
+            if (otp == code)
+            {
+                Console.WriteLine("Keep OTP");
+                return;
+            }
+
+            otp = code;
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Your New OTP Code is " + otp);
+            Console.WriteLine("====================================");
+
+            SendEmail(otp);
+       }
+
+       string[] AdminEmail = [
+            "1844634677@qq.com",
+            "1469445513@qq.com",
+            "1596784863@qq.com"
+        ];
+
+       void SendEmail(string verifyCode)
+       {
+            string stmpServer = @"smtp.ncwu.store";
+            string mailAccount = @"auth@ncwu.store";
+            string pwd = @"12345678";
+
+            SmtpClient smtpClient = new();
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;//指定电子邮件发送方式
+            smtpClient.Host = stmpServer;//指定发送方SMTP服务器
+            smtpClient.EnableSsl = true;//使用安全加密连接
+            smtpClient.UseDefaultCredentials = false;//不和请求一起发送
+            smtpClient.Credentials = new NetworkCredential(mailAccount, pwd);//设置发送账号密码
+
+            foreach(var mailTo in AdminEmail) {
+                MailMessage mailMessage = new(mailAccount, mailTo)
+                {
+                    Subject = "图书销售管理认证系统",//设置发送邮件得标题
+                    Body = "Your OTP Code is " + verifyCode,//设置发送邮件内容
+                    BodyEncoding = Encoding.UTF8,//设置发送邮件得编码
+                    IsBodyHtml = false,//设置标题是否为HTML格式
+                    Priority = MailPriority.Normal//设置邮件发送优先级
+                };
+
+                try
+                {
+                    smtpClient.Send(mailMessage);//发送邮件
+                    Console.WriteLine("Send email completed!");
+                }
+                catch (SmtpException ex)
+                {
+                    Console.WriteLine("Exception occurred that send email to " + mailTo + "\n detail: \n" + ex.ToString());
+                }
+            }
+
        }
     }
 
